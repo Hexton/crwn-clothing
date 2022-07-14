3d167429ba3961596c9985f1aa0255a33f86c3a6
Hexton Ho <hextonho@icloud.com>
2022-07-15 01:03:06 +0800

    Add navigation with router outlet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from './routers/home/home.component';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>This is Navigation</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return <h1>This is Shop</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
