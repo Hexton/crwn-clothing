de2a690bee9ed900fb385365ba0e2a14d5b1ab37
Hexton Ho <hextonho@icloud.com>
2022-07-15 01:59:12 +0800

    Style navigation and add logo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routers/home/home.component';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>This is Navigation</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import Navigation from './routers/navigation/navigation.component';
 
 const Shop = () => {
   return <h1>This is Shop</h1>;
